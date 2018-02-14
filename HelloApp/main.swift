fa97d6ed79c22969fd15525261d7fe84c47f1734
Parikh Dhruvin S <n01150847@nl-00115-012.local>
2018-02-14 08:36:39 -0500

    Initial Commit

---
+++ HelloApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloApp
+//
+//  Created by Parikh Dhruvin S. on 2/14/18.
+//  Copyright © 2018 Parikh Dhruvin S. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
