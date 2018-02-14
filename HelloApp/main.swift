a1b62e9e922099521e3c9d194ca4ab4468d0e124
Dhruvin Parikh <n01150847@humbermail.ca>
2018-02-14 09:43:58 -0500

    First Commit

--- HelloApp/main.swift
+++ HelloApp/main.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
-
+//This is demo App
+/*
+ This is swift programming
+ Today is week 03
+ This is an example of Multiline comment
+ */
 print("Hello, World!")
+print("Welcome to the swift programming")
+
+var number1 = 14;
+var student_id = 1001;
+var account:Int = 1234;
+print(number1);
+print(student_id);
+print(account);
+print(10+20);print(30-5);print(5*6);
+print("The maximum int value is \(Int.min)")
+print("The maximum Int value is \(Int.max)")
+var firstName = "Dhruvin";
+var lastName = "Parikh";
+print(firstName,lastName);
 
